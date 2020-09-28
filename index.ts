3e8d5005b0ce1abebaa66096fc9a3d65b188ce20
pkhruasu-ui <pkhruasu@gmail.com>
2020-09-27 23:00:44 -0500

    update code

--- index.ts
+++ index.ts
@@ -2,4 +2,12 @@ function plusthis(a,b) {
     return a+b;
 }
 
+exports.minus = function(a,b) {
+    return a-b;
+}
+
+exports.multiply = function(a,b) {
+    return a*b;
+}
+
 export default plusthis;
